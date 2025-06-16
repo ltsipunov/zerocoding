022ea9d3cb4bf9d53ebe9759b668feb73b865952
ltsipunov <l.tsipunov@yandes.ru>
2025-06-16 21:47:31 +0300

    ver 2.0 (with DJ3 models)

--- homeworks/DJ04_django_forms/movie_project/movie_project/urls.py
+++ homeworks/DJ04_django_forms/movie_project/movie_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('films.urls'))
+    path('', include('films.urls')),
+    path('news/', include('news.urls') )
 ]
