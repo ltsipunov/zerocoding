d271141182666f93e18420bb4f0202cceb1a0c76
ltsipunov <l.tsipunov@yandes.ru>
2025-06-30 21:00:04 +0300

    ver 3.1 news details added

--- homeworks/DJ04_django_forms/movie_project/news/urls.py
+++ homeworks/DJ04_django_forms/movie_project/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='news_home'),
-	path('fill', views.fill, name='news_fill')
+	path('fill', views.fill, name='news_fill'),
+	path('details/<int:id_value>/', views.details, name='news_details')
 ]
